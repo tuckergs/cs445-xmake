6345af92d1c3c96ba8925d476e0560e5d36b552e
Gabriel Tucker <tuckergs@clarkson.edu>
2017-05-08 02:20:07 -0400

    Added main.h, modified GRAMMAR and main.c
    At one point, we said that in the grammar that the grammar should handle text.
    We forgot to say in our GRAMMAR file that that shouldn't happen.
    Also, I added the execstack declaration

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+
+#include "main.h"
+
 #include <stdio.h>
 #include "lexer.h"
 #include "parser.h"
@@ -5,18 +8,13 @@
 
 int main()
 {
+  //Initialize the lexer
   initlexer();
+
+  //Begin parsing
   document();
 
-  /*
-  while(1)
-  {
-    int tok = _next_token();
-    if(tok <= 255)
-      fprintf(stderr,"{\'%c\'}",(char)tok);
-    else
-      fprintf(stderr,"{%s: %s}",tok2str(tok),getcurtext());
+  //Execute commands
+  execstack();
 
-  }
-  */
 }
---
+++ main.h
@@ -0,0 +1,14 @@
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+int main();
+
+void execstack();
+
+Stack tarstack;
+Stack cmdstack;
+
+
+
+#endif //MAIN_H_
